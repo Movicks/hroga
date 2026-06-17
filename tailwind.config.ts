789b67a1ee02188345f5afd7889e0087595bda14
Movicks <victoride013@gmail.com>
2026-06-17 20:14:39 +0100

    font changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+// import { Cormorant_Garamond } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -11,6 +12,10 @@ const config: Config = {
         primary: "#00BFFF", // Sky Blue
         secondary: "#1E3A8A", // Navy Blue Tint
         darkNavy: "#000080", // Dark Navy Blue
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        playfair: ["Playfair Display"],
       },
     },
   },
